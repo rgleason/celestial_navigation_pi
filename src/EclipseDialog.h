28f8620e88551aad25b624fa58c4e559b3ccf5f1
Paul OBrien <leblanc_leblanc@hotmail.com>
2026-09-07 00:56:02 +0100

    Revert "Change EVT_Download"
    This reverts commit 94db9279d6412e10a5e1fb9a9d9349dfcb795100.

--- src/EclipseDialog.h
+++ src/EclipseDialog.h
@@ -23,9 +23,6 @@ class wxStaticText;
 class wxTextCtrl;
 class celestial_navigation_pi;
 
-extern const wxEventTypeTag<wxCommandEvent> EVT_ECLIPSE_DOWNLOAD;
-
-
 class EclipseDialog : public wxDialog {
 public:
   EclipseDialog(wxWindow* parent, celestial_navigation_pi* plugin);
@@ -103,8 +100,6 @@ private:
   wxButton* m_download_de;
   wxButton* m_optional_data;
   wxButton* m_cancel_install;
-
- 
 
   std::vector<celestial_navigation::EclipseDataKind> m_install_queue;
   std::vector<std::string> m_download_sources;
