05c7f1e0214701fa3f9b4ba5be4fc8a9c584191a
Rick Gleason <fcgleason@tgp-architects.com>
2026-09-05 22:23:50 -0400

    CNDialog CN_pi Sight.h

--- src/Sight.h
+++ src/Sight.h
@@ -62,7 +62,7 @@ WX_DECLARE_LIST(wxRealPoint, wxRealPointList);
 const wxString SightType[] = {_("Altitude"), _("Azimuth"), _("Lunar"),
                               _("Horizon")};
 
-class Sight : public wxObject {
+class Sight {
 public:
   enum Type { ALTITUDE, AZIMUTH, LUNAR, HORIZON };
   enum HorizonEvent { SUNRISE, SUNSET };
